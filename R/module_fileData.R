793f76c38cfe003d6faf65dab0bfdc307f773cc3
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2019-07-15 19:39:46 +0200

    Start to clean fileData()

--- R/module_fileData.R
+++ R/module_fileData.R
@@ -12,30 +12,29 @@ fileDataUI <- function(id)
 # fileData ---------------------------------------------------------------------
 fileData <- function(input, output, session, path, file_data)
 {
-  # provide_data -----------------------------------------------------------------
-  provide_data <- function(x, input)
-  {
-    remove_common <- kwb.utils::selectElements(input, "remove_common_root")
-    keep_first <- kwb.utils::selectElements(input, "keep_first_root")
-    type <- kwb.utils::selectElements(input, "type_filter")
-    pattern <- kwb.utils::selectElements(input, "path_filter")
-    
-    if (remove_common) {
-      x$path <- kwb.file::remove_common_root(x$path, n_keep = 0 + keep_first)
-    }
-    
-    if (type != "all") {
-      x <- x[x$type == type, ]
-    }
-    
-    if (kwb.utils::defaultIfNULL(pattern, "") != "") {
-      x <- x[grepl(pattern, x$path), ]
-    }
-    
-    x
+  #provide_data <- function(x, input)
+  #{
+  get <- kwb.utils::selectElements
+  
+  keep_first <- get(input, "keep_first_root")
+  type <- get(input, "type_filter")
+  pattern <- get(input, "path_filter")
+  
+  if (get(input, "remove_common_root")) {
+    x$path <- kwb.file::remove_common_root(x$path, n_keep = 0 + keep_first)
   }
   
+  if (type != "all") {
+    x <- x[x$type == type, ]
+  }
   
+  if (kwb.utils::defaultIfNULL(pattern, "") != "") {
+    x <- x[grepl(pattern, x$path), ]
+  }
+  
+  x
+  #}
+
   output$file <- shiny::renderPrint({
     writeLines(
       sprintf("%d paths have been read from the file.", nrow(file_data()))

cbbd8272c1ca2a81eab3593a8951b484a05cfe70
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2019-07-18 15:05:35 +0200

    Do not fail if the user is unknown

--- R/app.R
+++ R/app.R
@@ -10,9 +10,15 @@
 # get_global -------------------------------------------------------------------
 get_global <- function(name)
 {
+  user <- try(kwb.utils::user())
+  
+  if (inherits(user, "try-error")) {
+    user <- "unknown"
+  }
+  
   globals <- list(
     max_plots = 5,
-    path_database = if (kwb.utils::user() == "hsonne") {
+    path_database = if (user == "hsonne") {
       "//medusa/processing/CONTENTS/file-info_by-department/2019-07"
     } else {
       "~/Desktop/Data/FAKIN/file-info_by-department"

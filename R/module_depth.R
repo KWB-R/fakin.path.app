1f272f9da0df4c96a2df049ef825bf4ac60a0e4b
hsonne <hauke.sonnenberg@gmx.de>
2019-07-24 14:28:29 +0200

    Remove old code sections that were commented out

--- R/module_depth.R
+++ R/module_depth.R
@@ -5,21 +5,6 @@ depthUI <- function(id)
   
   shiny::tagList(
     shiny::fluidRow(
-      # shiny::column(4, shiny::sliderInput(
-      #   inputId = ns("n_root_parts"),
-      #   label = "Root folder levels",
-      #   min = 1, max = 3, step = 1, value = 2
-      # )),
-      # shiny::column(4, inlineRadioButtons(
-      #   inputId = ns("group_aesthetics"),
-      #   label = "Group aesthetics",
-      #   choices = c("colour", "shape")
-      # )),
-      # shiny::column(4, inlineRadioButtons(
-      #   inputId = ns("group_by"),
-      #   label = "Group by",
-      #   choices = c("top-level" = "level-1", "extension")
-      # )),
       shiny::column(4, shiny::sliderInput(
         inputId = ns("n_sample"), label = "Sample size", 
         min = 5000L, max = 50000L, value = 5000L, step = 5000L
@@ -29,62 +14,15 @@ depthUI <- function(id)
         min = 3L, max = 8L, value = 5L, step = 1L
       ))
     ),
-    #shiny::plotOutput(ns("plot"))
     plotly::plotlyOutput(ns("plot"))
   )
-  # shiny::sidebarLayout(
-  #   shiny::sidebarPanel(
-  #     width = get_global("sidebar_width"),
-  #     shiny::sliderInput(
-  #       inputId = ns("n_root_parts"), 
-  #       label = "Root folder levels", 
-  #       min = 1, max = 3, step = 1, value = 2
-  #     ),
-  #     shiny::radioButtons(
-  #       inputId = ns("group_aesthetics"),
-  #       label = "Group aesthetics",
-  #       choices = c("colour", "shape")
-  #     ),
-  #     shiny::radioButtons(
-  #       inputId = ns("group_by"),
-  #       label = "Group by",
-  #       choices = c("extension", "level-1")
-  #     )
-  #   ), 
-  #   shiny::mainPanel(
-  #     shiny::plotOutput(ns("plot"))
-  #   )
-  # )
 }
 
 # depth ------------------------------------------------------------------------
 depth <- function(input, output, session, path_data)
 {
-  # file_data <- shiny::reactive({
-  #   
-  #   stopifnot(inherits(path_data(), "pathlist"))
-  #   
-  #   cbind.data.frame(
-  #     path_data()@data, 
-  #     path = as.character(path_data()), 
-  #     stringsAsFactors = FALSE
-  #   )
-  # })
-  
-  #output$plot <- shiny::renderPlot({
   output$plot <- plotly::renderPlotly({
-    #   prepared_data <- kwb.fakin:::prepare_for_scatter_plot2(
-    #     file_data = path_data()
-    #   )
-    #   
-    #   kwb.fakin:::plot_file_size_in_depth(
-    #     df = prepared_data,
-    #     main = "Total",
-    #     group_aesthetics = input$group_aesthetics,
-    #     group_by = input$group_by
-    #   )
-    # })
-    
+
     pl <- path_data()
     
     stopifnot(inherits(pl, "pathlist"))

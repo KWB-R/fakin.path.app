e196a0b378a2ed2bb919e2732386d9dab2ec67b4
hsonne <hauke.sonnenberg@gmx.de>
2019-07-24 11:09:12 +0200

    Add three wordclouds as one R plot with mfrow = c(1,3)

--- R/module_wordcloud.R
+++ R/module_wordcloud.R
@@ -3,29 +3,32 @@ wordcloudUI <- function(id)
 {
   ns <- shiny::NS(id)
   
+  right_border_column <- function(width, output) {
+    shiny::column(width, output, align = "center"
+      #, style = 'border-right: 1px solid red'
+    )
+  }
+  
   shiny::sidebarLayout(
     
     shiny::sidebarPanel(
       width = 3,
       shiny::sliderInput(ns("top_n"), "Number of top extensions", 5, 50, 10, 1),
-      # shiny::sliderInput(ns("scale_1"), "wordcloud: scale[1]", 1, 10, 4, 0.1),
-      # shiny::sliderInput(ns("scale_2"), "wordcloud: scale[2]", 1, 10, 0.5, 0.1),
-      # shiny::sliderInput(ns("min_freq"), "wordcloud: min.freq", 1, 1000, 3, 1),
-      # shiny::sliderInput(ns("max_words"), "wordcloud: max.words", 1, 1000, 1000, 1),
-      shiny::sliderInput(ns("size"), "wordcloud2: size", 0, 2, 1, 0.1),
-      shiny::sliderInput(ns("minSize"), "wordcloud2: minSize", 0, 2, 0, 0.1),
-      shiny::sliderInput(ns("gridSize"), "wordcloud2: gridSize", 0, 2, 0, 0.1)
+      shiny::sliderInput(ns("min_cex"), "Min. cex", 0.5, 2, 1, 0.1),
+      shiny::sliderInput(ns("multiple"), "Max. cex Multiple", 2, 10, 4, 1)
     ),
     
     shiny::mainPanel(
-      #shiny::fluidRow(
-        shiny::column(4, shiny::tableOutput(ns("table_extensions")))
-        , shiny::column(4, shiny::tableOutput(ns("table_folder_words")))
-        , shiny::column(4, shiny::tableOutput(ns("table_file_words")))
-        #, shiny::column(2, shiny::plotfOutput(ns("cloud1"))),
-        #, shiny::column(6, wordcloud2::wordcloud2Output(ns("cloud2")))
+      #style = 'border-right: 1px solid red',
+      shiny::fluidRow(
+        shiny::column(12, shiny::plotOutput(ns("plot")))
+      ),
+      shiny::fluidRow(
+        right_border_column(4, shiny::tableOutput(ns("table_extensions"))), 
+        right_border_column(4, shiny::tableOutput(ns("table_folder_words"))), 
+        right_border_column(4, shiny::tableOutput(ns("table_file_words")))
       )
-    #)
+    )
   )
 
 }
@@ -42,7 +45,7 @@ wordcloud <- function(input, output, session, path_data)
       return(dummy_data_frame)
     }
     freq_data <- kwb.utils::asNoFactorDataFrame(table(x))
-    head(freq_data[order(freq_data$Freq, decreasing = TRUE), ], top_n)
+    utils::head(freq_data[order(freq_data$Freq, decreasing = TRUE), ], top_n)
   }
   
   extract_words <- function(x) {
@@ -50,63 +53,62 @@ wordcloud <- function(input, output, session, path_data)
     grep("^[a-zA-Z]{3,}$", words, value = TRUE)
   }
 
+  file_types <- shiny::reactive({
+    if (length(path_data()) == 0) {
+      character()
+    } else {
+      kwb.utils::selectColumns(path_data()@data, "type")
+    }
+  })
+
+  indices_directory <- shiny::reactive({
+    which(file_types() == "directory")
+  })
+
+  indices_file <- shiny::reactive({
+    which(file_types() == "file")
+  })
+  
   folder_word_freq <- shiny::reactive({
 
-    result <- if (length(path_data()) == 0) {
-      dummy_data_frame
-    } else {
-      
-      kwb.utils::printIf(TRUE, length(path_data))
-      
-      types <- kwb.utils::selectColumns(path_data()@data, "type")
-      
-      kwb.utils::printIf(TRUE, head(types))
-      
-      indices_directory <- which(types == "directory")
-      
-      kwb.utils::printIf(TRUE, head(indices_directory))
-      
-      if (length(indices_directory)) {
-        
-        kwb.utils::printIf(TRUE, length(indices_directory))
-        
-        folder_names <- pathlist::filename(path_data()[indices_directory])
-        top_n_freq_data(extract_words(folder_names), input$top_n)
-      } else {
-        dummy_data_frame
-      }
-    }
+    indices <- indices_directory()
 
-    stats::setNames(result, c("Word in directory name", "Count"))
+    result <- if (length(indices)) {
+      folder_names <- pathlist::filename(path_data()[indices])
+      top_n_freq_data(extract_words(folder_names), input$top_n)
+    } else {
+      dummy_data_frame
+    }
+    
+    stats::setNames(result, c("Word in folder name", "Count"))
   })
  
   file_word_freq <- shiny::reactive({
     
-    result <- if (length(path_data()) == 0) {
-      dummy_data_frame
+    indices <- indices_file()
+    
+    result <- if (length(indices)) {
+      filenames <- pathlist::filename(path_data()[indices])
+      filenames <- kwb.utils::removeExtension(filenames)
+      top_n_freq_data(extract_words(filenames), input$top_n)
     } else {
-      indices_file <- which(path_data()@data$type == "file")
-      if (length(indices_file)) {
-        filenames <- pathlist::filename(path_data()[indices_file])
-        filenames <- kwb.utils::removeExtension(filenames)
-        top_n_freq_data(extract_words(filenames), input$top_n)
-      } else {
-        dummy_data_frame
-      }
+      dummy_data_frame
     }
     stats::setNames(result, c("Word in filename", "Count"))
   })
 
   extension_freq <- shiny::reactive({
 
-    result <- if (length(path_data()) == 0) {
-      dummy_data_frame
-    } else {
-      extensions <- kwb.utils::fileExtension(pathlist::filename(path_data()))
-      print(table(extensions))
-      freq_data <- top_n_freq_data(x = extensions, top_n = input$top_n)
+    indices <- indices_file()
+    
+    result <- if (length(indices)) {
+      filenames <- pathlist::filename(path_data()[indices])
+      extensions <- kwb.utils::fileExtension(filenames)
+      freq_data <- top_n_freq_data(extensions, input$top_n)
       freq_data[kwb.utils::isNaOrEmpty(freq_data[, 1]), 1] <- "<none>"
       freq_data
+    } else {
+      dummy_data_frame
     }
 
     stats::setNames(result, c("Filename extension", "Count"))
@@ -115,19 +117,40 @@ wordcloud <- function(input, output, session, path_data)
   output$table_extensions <- shiny::renderTable(extension_freq())
   output$table_folder_words <- shiny::renderTable(folder_word_freq())
   output$table_file_words <- shiny::renderTable(file_word_freq())
-  # 
-  # # output$cloud1 <- shiny::renderPlot({
-  # #   wordcloud::wordcloud(
-  # #     extension_freq()$Var1, extension_freq()$Freq,
-  # #     scale = c(input$scale_1, input$scale_2), min.freq = input$min_freq,
-  # #     max.words = input$max_words
-  # #   )
-  # # })
-  # 
-  # output$cloud2 <- wordcloud2::renderWordcloud2({
-  #   wordcloud2::wordcloud2(
-  #     extension_freq(), size = input$size, minSize = input$minSize,
-  #     gridSize = input$gridSize
-  #   )
-  # })
+  
+  output$plot <- shiny::renderPlot({
+    old_par <- graphics::par(mfrow = c(1, 3), mar = c(0, 0, 3, 0))
+    on.exit(graphics::par(old_par))
+    
+    min_cex <- input$min_cex
+    multiple <- input$multiple
+    
+    scale <- min_cex * c(multiple, 1)
+
+    freq_functions <- list(extension_freq, folder_word_freq, file_word_freq)               
+    titles <- c("Filename Extensions", "Words in Folder Names", "Words in Filenames")
+    
+    for (i in seq_along(titles)) {
+      plot_wordcloud_or_message(
+        freq = freq_functions[[i]](), main = titles[i], scale = scale
+      )
+    }
+  })
+}
+
+# plot_wordcloud_or_message ----------------------------------------------------
+plot_wordcloud_or_message <- function(
+  freq, main = "Wordcloud", scale = c(10, 0.5), failure_text = "Nothing to plot"
+)
+{
+  if (nrow(freq)) {
+    wordcloud::wordcloud(
+      words = freq[, 1], freq = freq[, 2], scale = scale, 
+      min.freq = min(freq[, 2])
+    )
+  } else {
+    plot_centered_message(failure_text)
+  }
+  
+  graphics::title(main, cex.main = 2)
 }

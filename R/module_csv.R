fc11e3ce430ff73e52a9d18370be5f36fd692fde
hsonne <hauke.sonnenberg@gmx.de>
2019-07-24 23:28:59 +0200

    Save paths loaded and split once as rds file

--- R/module_csv.R
+++ R/module_csv.R
@@ -34,7 +34,22 @@ csvFile <- function(input, output, session, read_function)
 {
   file_path <- shiny::reactive(input$file)
 
+  rds_file <- shiny::reactive({
+    gsub("\\.csv$", ".rds", input$file)
+  })
+  
+  rds_file_exists <- shiny::reactive({
+    result <- file.exists(rds_file())
+    cat("rds file ", rds_file(), "exists:", result, "\n")
+    result
+  })
+  
   raw_content <- shiny::reactive({
+    
+    if (rds_file_exists()) {
+      return(function(...) NULL)
+    } 
+
     file <- file_path()
     x <- kwb.fakin::read_file_paths(file)
     x <- kwb.utils::renameColumns(x, list(
@@ -45,14 +60,36 @@ csvFile <- function(input, output, session, read_function)
     kwb.utils::selectColumns(x, c("path", "type", "size", "modified"))
   })
 
+  rds_content <- shiny::reactive({
+    if (rds_file_exists()) {
+      readRDS(rds_file())
+    } else {
+      NULL
+    }
+  })
+  
   path_list <- shiny::reactive({
-    pathlist::pathlist(
+    
+    if (! is.null(rds_content())) {
+      return(rds_content()$path_list)
+    }
+
+    pl <- pathlist::pathlist(
       paths = raw_content()$path, 
       data = raw_content()[, c("type", "size")]
     )
+    
+    pl@root <- hide_server(pl@root)
+    
+    pl
   })
   
   content <- shiny::reactive({
+    
+    if (! is.null(rds_content())) {
+      return(rds_content()$content)
+    }
+    
     x <- raw_content()
     dates <- as.Date(as.POSIXct(x$modified, "%Y-%m-%dT%H:%M:%S", tz = "UTC"))
     x$modified <- dates
@@ -70,7 +107,12 @@ csvFile <- function(input, output, session, read_function)
       "toplevel", "folder", "filename", "extension"
     ))
     
-    structure(x, root = path_list()@root)
+    content <- structure(x, root = path_list()@root)
+
+    rds_content <- list(content = content, path_list = path_list())
+    saveRDS(rds_content, file = rds_file())
+    
+    content
   })
   
   list(file = file_path, content = content, path_list = path_list)

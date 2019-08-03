ce50683788caaf7c41b577a4503c15419209efcc
hsonne <hauke.sonnenberg@gmx.de>
2019-08-03 13:11:52 +0200

    Improve setting of global variables
    Also simplify naming

--- R/app.R
+++ R/app.R
@@ -24,21 +24,21 @@ get_ui <- function() shiny::fluidPage(
 # Define server logic ----------------------------------------------------------
 server <- function(input, output)
 {
-  filtered_path_list <- shiny::callModule(fileData, "id_fileData")
+  path_list <- shiny::callModule(fileData, "id_fileData")
   
   # Call the sankey module. The id needs to be passed to the server function.
   # It is required to name the output that is generated dynamically.
   # Take care: do not use a helper variable to pass the id but a constant 
   # string, as in the following 
   shiny::callModule(
-    sankey, "id_sankey", path_list = filtered_path_list, id = "id_sankey"
+    sankey, "id_sankey", path_list = path_list, id = "id_sankey"
   )
   
-  shiny::callModule(explore, "id_explore", path_data = filtered_path_list)
-  shiny::callModule(mytreemap, "id_treemap", path_data = filtered_path_list)
-  shiny::callModule(depth, "id_depth", path_data = filtered_path_list)
-  shiny::callModule(stats, "id_stats", path_data = filtered_path_list)
-  shiny::callModule(wordcloud, "id_wordcloud", path_data = filtered_path_list)
+  shiny::callModule(explore, "id_explore", path_data = path_list)
+  shiny::callModule(mytreemap, "id_treemap", path_data = path_list)
+  shiny::callModule(depth, "id_depth", path_data = path_list)
+  shiny::callModule(stats, "id_stats", path_data = path_list)
+  shiny::callModule(wordcloud, "id_wordcloud", path_data = path_list)
 }
 
 # run_app(): Run the application -----------------------------------------------
@@ -47,16 +47,14 @@ server <- function(input, output)
 #' 
 #' @param path_database if not \code{NULL} the path to a folder containing 
 #'   text files with path information
-#' @param reactlog logical. The shiny option \code{shiny.reactlog} is set 
-#'   accordingly before starting the app
+#' @param \dots further \code{key = value} pairs to be used as global variables
 #' @export
-run_app <- function(path_database = NULL, reactlog = FALSE)
+run_app <- function(path_database = NULL, ...)
 {
-  if (! is.null(path_database)) {
-    options(fakin.path.app.path_database = path_database)
-  }
-  
-  options(shiny.reactlog = reactlog) 
-  
+  # Set defaults (1. hard coded, 2. from environment variables, 3. from user)
+  set_global(list. = default_globals())
+  set_global(list. = get_environment_vars("^PATHANA_"))
+  set_global(path_database = path_database, ...)
+
   shiny::shinyApp(ui = get_ui(), server = server)
 }
